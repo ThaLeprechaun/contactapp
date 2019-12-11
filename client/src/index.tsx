abff16b3e98b608784515ab4c6f6700c16979998
thaleprechaun <oakinbiyiusman@gmail.com>
2019-12-11 12:18:13 +0100

    Import App

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { RestfulProvider } from 'restful-react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import 'semantic-ui-css/semantic.min.css';
+
+ReactDOM.render(
+  <RestfulProvider base="/">
+    <Router>
+      <App />
+    </Router>
+  </RestfulProvider>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
